a0e7737f5cf9bf2a1f54747f2e3e67cdd5e07718
rubenaryo <rubeny484@gmail.com>
2022-10-01 09:17:31 -0700

    Sandbag Actor Parent Class setup

--- Source/FightTraining/SandbagActor.cpp
+++ Source/FightTraining/SandbagActor.cpp
@@ -3,12 +3,33 @@
 
 #include "SandbagActor.h"
 
+#include "Components/CapsuleComponent.h"
+
 // Sets default values
 ASandbagActor::ASandbagActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Add the capsule component
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	CapsuleComponent->SetupAttachment(RootComponent);
+
+	// Create Default Mesh
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	if (Mesh)
+	{
+		Mesh->AlwaysLoadOnClient = true;
+		Mesh->AlwaysLoadOnServer = true;
+		Mesh->bOwnerNoSee = false;
+		Mesh->bCastDynamicShadow = true;
+		Mesh->bAffectDynamicIndirectLighting = true;
+		Mesh->PrimaryComponentTick.TickGroup = TG_PrePhysics;
+		Mesh->SetupAttachment(CapsuleComponent);
+		Mesh->SetCollisionProfileName(TEXT("CharacterMesh"));
+		Mesh->SetGenerateOverlapEvents(true);
+		Mesh->SetCanEverAffectNavigation(false);
+	}
 }
 
 // Called when the game starts or when spawned
--- Source/FightTraining/SandbagActor.h
+++ Source/FightTraining/SandbagActor.h
@@ -6,10 +6,21 @@
 #include "GameFramework/Actor.h"
 #include "SandbagActor.generated.h"
 
+class UCapsuleComponent;
+
 UCLASS()
 class FIGHTTRAINING_API ASandbagActor : public AActor
 {
 	GENERATED_BODY()
+
+private:
+	/** The CapsuleComponent being used for movement collision **/
+	UPROPERTY(Category = Collision, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCapsuleComponent> CapsuleComponent;
+
+	/** The main skeletal mesh associated with this Sandbag Actor **/
+	UPROPERTY(Category = Actor, VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> Mesh;
 	
 public:	
 	// Sets default values for this actor's properties
@@ -23,4 +34,9 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	/** Returns Mesh subobject **/
+	FORCEINLINE const UStaticMeshComponent* GetMesh() const { return Mesh; }
+
+	/** Returns CapsuleComponent subobject **/
+	FORCEINLINE const UCapsuleComponent* GetCapsuleComponent() const { return CapsuleComponent; }
 };
