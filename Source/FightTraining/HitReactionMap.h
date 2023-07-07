b0456d3d4d83c23473917fd3543d7dcc84fa1902
rubenaryo <rubeny484@gmail.com>
2023-07-07 06:43:50 -0700

    Documentation Pass

--- Source/FightTraining/HitReactionMap.h
+++ Source/FightTraining/HitReactionMap.h
@@ -30,8 +30,10 @@ UCLASS(Blueprintable, ClassGroup = Combat)
 class UHitReactionMap : public UDataAsset
 {
     GENERATED_BODY()
+
     UHitReactionMap();
 
+    // Populates the hit section maps.
     virtual void PreSave(FObjectPreSaveContext ObjectSaveContext) override;
 
 public:
@@ -42,12 +44,21 @@ public:
     EHitSection GetHitSectionFromBoneName(const FName& boneName);
 
 private:
+    /// <summary>
+    /// Maps a body's hit section with the bones it represents
+    /// </summary>
     UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (DisplayName = "Hit Section to Bone Map", AllowPrivateAccess = "true"))
     TMap<TEnumAsByte<EHitSection>, FBoneNameArray> HitSectionToBoneMap;
 
+    /// <summary>
+    /// Opposite map to HitSectionToBoneMap. For a given bone, returns its hit section. 
+    /// </summary>
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (DisplayName = "Bone to Hit Section Map (generated)", AllowPrivateAccess = "true"))
     TMap<FName, TEnumAsByte<EHitSection>> BoneToHitSectionMap;
     
+    /// <summary>
+    /// For a given hit section, returns the reactions that can play from tuned reference vectors.
+    /// </summary>
     UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (DisplayName = "Hit Section to Reaction Map", AllowPrivateAccess = "true"))
     TMap<TEnumAsByte<EHitSection>, FHitReactionReferenceArray> HitSectionToReactionMap;
 
